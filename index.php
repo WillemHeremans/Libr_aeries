3289182cf988e189bd442e712e92ef3d7ccccca9
willemHeremans <willemheremans@gmail.com>
2017-10-18 16:42:33 +0200

    Libr_aeries

--- index.php
+++ index.php
@@ -11,32 +11,19 @@
 
 <body>
 
+<div class="parent">
 
-<section>
+	<div class="enfant" id="languages">
 
-
-	<div id="languages">
 		<ul>
 			<li class="lang"><img src="./images/logo.png" alt="logo"></li>
-			<li class="lang"><a href="index.php?lang=en">Welcome</a></li>
-			<li class="lang"><a href="index.php?lang=nl">Welkom</a></li>
-			<li class="lang"><a href="index.php?lang=fr">Bienvenue</a></li>
+			<li class="lang"><a href="menu.php?lang=en">Welcome</a></li>
+			<li class="lang"><a href="menu.php?lang=nl">Welkom</a></li>
+			<li class="lang"><a href="menu.php?lang=fr">Bienvenue</a></li>
 		</ul>
+
 </div>
-
-
-
-
-	<!-- <ul>
-		<li class="menu"><a href="/"><?php echo $lang['MENU_LITT']; ?></a></li>
-	  <li class="menu"><a href="about_us"><?php echo $lang['MENU_JEUN']; ?></a></li>
-		<li class="menu"><a href="our_products"><?php echo $lang['MENU_INT']; ?></a></li>
-	  <li class="menu"><a href="contact_us"><?php echo $lang['MENU_SCIENCES']; ?></a></li>
-	  <li class="menu"><a href="advertise"><?php echo $lang['MENU_BD']; ?></a></li>
-	  <li class="menu"><a href="sitemap"><?php echo $lang['MENU_CONTACT']; ?></a></li>
-	    </ul> -->
-
-
+</div>
 
 </body>
 </html>
---
+++ menu.php
@@ -0,0 +1,29 @@
+<?php include './languages/common.php';?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<link rel="stylesheet" href="./styles/menu.css"/>
+<title><?php echo $lang['PAGE_TITLE']; ?></title>
+
+</head>
+
+<body>
+
+
+  <ul class="menu">
+    <li class="menu" id="logo"><img src="./images/logo.png" alt="logo"></li>
+    <li class="menu" id="first-entry"><a href="/"><?php echo $lang['MENU_LITT']; ?></a></li>
+    <li class="menu"><a href="about_us"><?php echo $lang['MENU_JEUN']; ?></a></li>
+    <li class="menu"><a href="our_products"><?php echo $lang['MENU_INT']; ?></a></li>
+    <li class="menu"><a href="contact_us"><?php echo $lang['MENU_SCIENCES']; ?></a></li>
+    <li class="menu"><a href="advertise"><?php echo $lang['MENU_BD']; ?></a></li>
+    <li class="menu"><a href="sitemap"><?php echo $lang['MENU_CONTACT']; ?></a></li>
+      </ul>
+
+
+
+
+</body>
+</html>
